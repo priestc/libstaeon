a541a8be91c473b667d23d5b95016d3ce66f00e9
Chris Priest <cp368202@ohiou.edu>
2019-02-24 08:38:28 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import sys
+
+extra_install = []
+if sys.version_info <= (3,1):
+    extra_install.append('futures')
+
+setup(
+    name="staeon-core",
+    version='0.1.0',
+    description='Core library for the Staeon cryptocurrency',
+    long_description=open('README.md').read(),
+    author='Chris Priest',
+    author_email='nbvfour@gmail.com',
+    url='https://github.com/priestc/staeon-core',
+    packages=find_packages(),
+    scripts=['bin/staeon'],
+    include_package_data=True,
+    license='LICENSE',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'requests',
+        'arrow',
+        'bitcoin',
+    ] + extra_install
+)
