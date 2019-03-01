eb609d0887081b5578c4c5b50f36a6efe205b323
Chris Priest <cp368202@ohiou.edu>
2019-02-28 22:15:29 -0700

    added code to push a peer registration to nodes

--- staeon/peer_registration.py
+++ staeon/peer_registration.py
@@ -1,4 +1,6 @@
 import datetime
+import requests
+import json
 from bitcoin import ecdsa_verify, ecdsa_recover, ecdsa_sign, pubtoaddr, privtoaddr
 
 from .exceptions import InvalidSignature
@@ -35,15 +37,36 @@ def validate_peer_registration(reg, now=None):
     return True
 
 def get_peerlist():
+    """
+    Tries seed nodes until a peerlist is returned
+    """
+    response = None
     for seed in SEED_NODES:
         url = "http://%s/staeon/peerlist?top" % seed
+        print(url)
         try:
-            response = requests.get(url).response
-        except:
+            response = requests.get(url).json()
+        except (requests.exceptions.ConnectionError, ValueError) as exc:
+            print(exc)
             continue
+        break
 
-def push_peer_registration(reg, peers=None):
+    if not response:
+        raise Exception("Can't get peerlist")
+
+    return response['peers']
+
+
+def push_peer_registration(reg, peers=None, verbose=True):
     if not peers: peers = get_peerlist()
 
     for peer in peers:
-        peer
+        domain = peer['domain']
+        url = "http://%s/peerlist" % domain
+        if verbose: print("Pushing to: " + domain)
+        response = requests.post(url, {'registration': json.dumps(reg)})
+        if verbose: print("..." + response.content)
+
+def register_peer(domain, pk, peers=None):
+    reg = make_peer_registration(pk, domain)
+    push_peer_registration(reg, peers=peers)
