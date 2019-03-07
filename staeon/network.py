e3e979f8ec1fdc06e62e23b6bde2437ec85ccdfc
Chris Priest <cp368202@ohiou.edu>
2019-03-07 13:42:08 -0700

    added decimal activation code

--- staeon/network.py
+++ staeon/network.py
@@ -7,14 +7,31 @@ EPOCH_LENGTH_SECONDS = 600
 EPOCH_CLOSING_SECONDS = 10
 PROPAGATION_WINDOW_SECONDS = 10
 SEED_NODES = ['staeon.com', 'staeon.org']
-
-def emission(epoch):
+DECIMAL_ACTIVATION_POINTS = [
+    [8, 1], # 2019 AD
+    [9, 2879631], # 2073 AD
+    [10, 10913911], # 2226 AD
+    [11, 36472052], # 2712 AD
+    [12, 117342635], # 4250 AD
+    [13, 373093151], # 9112 AD
+    [14, 1181852257] # 24504 AD
+]
+def raw_emission(epoch):
     """
-    returns the block reward for given epoch.
+    Returns the block reward for given epoch,
+    not catenated with appropriate decimals
     """
     return 139899456000000000.0 / (
         10000 * epoch**2 + 18779955400 * epoch + 2897490120649729.0
     )
+
+def emission(epoch):
+    """
+    Epoch award, catenated to the appropriate decimal places.
+    """
+    raw = raw_emission(epoch)
+    template = "%%.%df" % get_decimals_for_epoch(epoch)
+    return float(template % raw)
 
 def total_supply_at(epoch):
     """
@@ -27,3 +44,12 @@ def total_supply_at(epoch):
             abs(100 * epoch - 1140000 * sq + 93899777) / abs(100 * epoch + 1140000 * sq + 93899777)
         )
     ) / (19 * sq) + (emission(epoch) / 2)
+
+def get_decimals_for_epoch(epoch):
+    ret = None
+    for decimal_count, activation_epoch in DECIMAL_ACTIVATION_POINTS:
+        if epoch >= activation_epoch:
+            ret = decimal_count
+        else:
+            break
+    return ret
--- staeon/utils.py
+++ staeon/utils.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 from staeon.consensus import get_epoch_range
 from staeon.network import emission, total_supply_at
 
-def make_emission_table(to_epoch=100000, fast=True):
+def make_emission_table(to_epoch=100000, accurate=False):
     """
     Make emission table including completely accurate supply.
     """
@@ -11,22 +11,22 @@ def make_emission_table(to_epoch=100000, fast=True):
     last = 0
 
     headers = ['epoch', 'time', 'reward', 'supply', 'added to supply']
-    if not fast:
+    if accurate:
         headers += ['accurate', 'error']
 
     for x in headers:
         print(x.ljust(19), end="")
     print("", end="\n")
 
-    if fast:
+    if not accurate:
         to_iterate = xrange(1, to_epoch, to_epoch/10)
         make_print = lambda epoch: True
     else:
         to_iterate = xrange(1, to_epoch)
-        make_print = lambda epoch: epoch == 1 or epoch % (to_epoch/10.0) == 0
+        make_print = lambda epoch: epoch % (to_epoch/10.0) == 0
 
     for epoch in to_iterate:
-        if not fast: cumm += emission(epoch)
+        if accurate: cumm += emission(epoch)
 
         if make_print(epoch):
             reward = emission(epoch)
@@ -44,13 +44,38 @@ def make_emission_table(to_epoch=100000, fast=True):
                 tsa,
                 tsa - last
             ]
-            if not fast:
+            if accurate:
                 data += [cumm, tsa - cumm]
 
             for x in data:
                 print(str(x).ljust(19), end="")
             print("", end="\n")
-            if not fast:
+
+            if accurate:
                 last = cumm
             else:
                 last = tsa
+
+
+def get_decimal_activation_epochs(start_epoch=1, end_epoch=9999999):
+    """
+    Find the epoch at which each decimal place is activated.
+    """
+    activation_emissions = [
+        [8, 100],
+        [9, 1.0],
+        [10, 0.1],
+        [11, 0.01],
+        [12, 0.001],
+        [13, 0.0001],
+        [14, 0.00001]
+    ]
+    activations = []
+    for epoch in xrange(start_epoch, end_epoch):
+        target_decimals, target_emission = activation_emissions[len(activations)]
+        if raw_emission(epoch) < target_emission:
+            print("[%d, %d]" % (target_decimals, epoch))
+            activations.append([target_decimals, epoch])
+            if len(activations) == len(activation_emissions):
+                break
+    return activations
